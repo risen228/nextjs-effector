ae68465ee9330b94553d4f6896e423b9b76ac9db
Evgeny Zakharov <risenforces@gmail.com>
2022-06-07 00:14:47 +0300

    fix: fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+import '@fontsource/acme'
+import '@fontsource/fira-mono'
 import '@app/shared/ui/globals.css'
 import App from 'next/app'
 import { withEffector } from '@app/shared/lib/effector'
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Acme&family=Fira+Mono&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
