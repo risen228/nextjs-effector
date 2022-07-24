bb36f1173f59520e6c7037d4bf1d765af1b60a12
Evgeny Zakharov <risenforces@gmail.com>
2022-07-24 22:15:55 +0300

    fix: installation script

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ wget -O $TEMP/archive.zip https://github.com/risenforces/nextjs-effector/archive
 mkdir $TEMP/extracted/
 tar -xf $TEMP/archive.zip -C $TEMP/extracted/
 rm -rf $NEXTJS_EFFECTOR_FOLDER
-mkdir $NEXTJS_EFFECTOR_FOLDER
+mkdir -p $NEXTJS_EFFECTOR_FOLDER
 cp -R $TEMP/extracted/nextjs-effector-release-latest/library/ $NEXTJS_EFFECTOR_FOLDER
 rm -rf $TEMP
