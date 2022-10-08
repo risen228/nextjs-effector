16646a9e83610af50a78e447f8d5f6fd6ba55e1f
Evgeny Zakharov <risenforces@gmail.com>
2022-10-08 17:09:12 +0300

    fix: effector and next integration issues

--- library/effector-react.ts
+++
@@ -1,9 +0,0 @@
-import * as effector from 'effector-react/scope'
-
-export type EffectorReactImports = typeof effector
-
-export const EffectorReact = {} as EffectorReactImports
-
-export function setEffectorReact(entities: EffectorReactImports) {
-  Object.assign(EffectorReact, entities)
-}
